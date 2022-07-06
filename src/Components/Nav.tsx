3b823abfc3f4ca6f2eaf504177c93c6152930571
Astro <astro@Astros-MacBook-Pro.local>
2022-07-06 23:57:36 +0100

    changed nav direction

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -22,14 +22,14 @@ interface Styles {
 
 const ScrollText = keyframes`
     from {
-        -moz-transform: translateX(-100%);
-        -webkit-transform: translateX(-100%);
-        transform: translateX(-100%);
-    }
-    to {
         -moz-transform: translateX(100%);
         -webkit-transform: translateX(100%);
         transform: translateX(100%);
+    }
+    to {
+        -moz-transform: translateX(-100%);
+        -webkit-transform: translateX(-100%);
+        transform: translateX(-100%);
     }
 `
 
